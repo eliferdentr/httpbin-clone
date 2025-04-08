099792f82369a9e4c65688ec392e3a439fc9dffd
Elif Erden <eliferden@hitab.net>
2025-04-08 14:19:55 +0300

    -

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	db "httbinclone-eliferden.com/db"
 	router "httbinclone-eliferden.com/routes"
 )
 
 func main() {
 	server := gin.Default()
 	router.RegisterRoutes(server)
-	db.InitDB()
 }
